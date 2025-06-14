1720a7b8f227dc6712ee16348bc4a7b32b6ff43c
Banzi Alvarez <ianbanzialvarezf@gmail.com>
2025-06-13 19:41:23 -0600

    Agregar archivos de proyecto.

---
+++ ClientePuntoDeVenta/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ClientePuntoDeVenta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
