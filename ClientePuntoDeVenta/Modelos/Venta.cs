e4e1400243250445f3bd8647db371c4cd3d2b2a6
Banzi Alvarez <ianbanzialvarezf@gmail.com>
2025-06-15 11:44:33 -0600

    Se añadieron clase de modelo para la funcionalidad del sistema

---
+++ ClientePuntoDeVenta/Modelos/Venta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientePuntoDeVenta.Modelos
+{
+    internal class Venta
+    {
+        public int Id { get; set; }
+        public List<VentaDetalle> Detalles { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ ClientePuntoDeVenta/Modelos/VentaDetalle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientePuntoDeVenta.Modelos
+{
+    internal class VentaDetalle
+    {
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
